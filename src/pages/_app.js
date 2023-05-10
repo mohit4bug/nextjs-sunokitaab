9f1437b0bfbb0e18c7b2d275f9fc7096bd60b270
Mohit Khatri <mohit4bug@gmail.com>
2023-05-10 16:36:42 +0530

    Font fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,3 @@
-import { Inter_Tight } from 'next/font/google'
-const font = Inter_Tight({
-  subsets: ['latin']
-})
 export default function App({ Component, pageProps }) {
   return (
     <Component {...pageProps} />
